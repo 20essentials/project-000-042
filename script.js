d4dafa72e92a63c54f5418269dee783e53917292
DominicDev <s0l0estudi4r@gmail.com>
2025-03-27 19:04:18 +0100

    v0.0.2

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-const d = document;
-
 let pairs = [
-    [90, -25],
-    [90, 0],
-    [90, 25],
-    [180, -25],
-    [180, 0],
-    [180, 25],
-  ],
-  i = -1;
+  [90, -25],
+  [90, 0],
+  [90, 25],
+  [180, -25],
+  [180, 0],
+  [180, 25]
+];
+let i = -1;
 
-d.addEventListener("click", (e) => {
-  if (e.target.matches(".box")) {
-    let element = e.target;
-    if (i <= 4) {
-      i++;
-    } else {
-      i = 0;
-    }
-    element.style.setProperty(
-      "transform",
+document.addEventListener('click', e => {
+  if (e.target.matches('.box')) {
+    if (i <= 4) i++;
+    else i = 0;
+    e.target.style.setProperty(
+      'transform',
       `rotateZ(${pairs[i][0]}deg) rotateY(${pairs[i][1]}deg)`
     );
   }
